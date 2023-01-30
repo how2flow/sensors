7a40a5fa95f0b19407af8573b64967df18eca371
Steve Jeong <how2soft@gmail.com>
2023-01-30 11:31:36 +0900

    kit/src: modify pwm example (KY-009, KY-016).
    Added a simple patterns.
    Change pwm pins Where both N2 and C4 are compatible.
    
    Signed-off-by: Steve Jeong <how2soft@gmail.com>
    Change-Id: I173b2f23888c25791272d5d850703c580e081cd4

--- kit/src/pwm/ky-016.c
+++ kit/src/pwm/ky-016.c
@@ -1,12 +1,14 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
-#define rPIN 23 // phy #33
-#define gPIN 3  // phy #15
-#define bPIN 7  // phy #7
+#define rPIN 24 // phy #35
+#define gPIN 23  // phy #33
+#define bPIN 3  // phy #15
+
+#define DELAY 10
 
 /* target:
- * ky-016, ky-009
+ * ky-009, ky-016
  */
 
 int main()
@@ -20,11 +22,31 @@ int main()
 	pwmSetClock(2400);
 	pwmSetRange(100);
 
-	for (value = 100; value > 0; value--) {
-		pwmWrite(rPIN, value);
-		pwmWrite(gPIN, (100 - value));
-		pwmWrite(bPIN, (value / 2));
-		delay(5);
+	pwmWrite(rPIN, 0);
+	pwmWrite(gPIN, 0);
+	pwmWrite(bPIN, 0);
+
+	while (1) {
+		for (value = 100; value > 0; value--) {
+			pwmWrite(rPIN, value);
+			pwmWrite(gPIN, (100 - value));
+			pwmWrite(bPIN, 0);
+			delay(DELAY);
+		}
+
+		for (value = 100; value > 0; value--) {
+			pwmWrite(rPIN, (100 - value));
+			pwmWrite(gPIN, 0);
+			pwmWrite(bPIN, value);
+			delay(DELAY);
+		}
+
+		for (value = 100; value > 0; value--) {
+			pwmWrite(rPIN, 0);
+			pwmWrite(gPIN, value);
+			pwmWrite(bPIN, (100 - value));
+			delay(DELAY);
+		}
 	}
 	return 0;
 }
