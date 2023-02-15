7db69ed9c8824759b16bdec032488b29aee6fd91
Steve Jeong <how2soft@gmail.com>
2023-02-15 10:30:44 +0900

    kit/src: Improve analog and gpio codes.
    In "analog-and-digital-input.c",
    
    analog was incorporated as "analog-input.c" and digital as "gpio-input.c".
    
    Signed-off-by: Steve Jeong <how2soft@gmail.com>
    Change-Id: Ic174344d00182e23a798b8932ec3ca6e4081542c

--- kit/src/analog/analog-input.c
+++ kit/src/analog/analog-input.c
@@ -1,30 +1,56 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
-#define PIN 29
+#define SENSING_RANGE 50
+
+#define ADC_MAX 4095 // 4KB - 1
+#define ADC_MIN (SENSING_RANGE + 5)
+
+#define ADC 29
 #define LED 24
 
 /* target:
  * ky-013, ky-018,
- * ky-033, ky-035
- */ 
+ * ky-024, ky-025,
+ * ky-026, ky-033,
+ * ky-035, ky-036,
+ * ky-037, ky-038
+ */
 
 int main()
 {
-	int value;
+	int value, prev_value;
+	int sensing;
 
 	wiringPiSetup();
 	pinMode(LED, OUTPUT);
 
+	prev_value = analogRead(ADC);
+
 	while (1) {
-		value = analogRead(PIN);
-		if (value > 512) {
-			digitalWrite(LED, HIGH);
+		value = analogRead(ADC);
+		sensing = prev_value - value;
+
+		if (value >= ADC_MAX) {
+			printf("turn volume to CW.\n");
+			continue;
 		}
-		else {
-			digitalWrite(LED, LOW);
+
+		if (value < ADC_MIN) {
+			printf("turn volume to CCW.\n");
+			continue;
 		}
-		delay(5);
+
+		if (sensing >= SENSING_RANGE) {
+			printf("signal detected.\n");
+		}
+
+		/* debugging */
+		// printf("analog value: %d\n", value);
+
+		prev_value = value;
+		delay(10);
 	}
+
 	return 0;
 }
