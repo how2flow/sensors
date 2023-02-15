de6e4fc9f6bce9d2f7068b3269d87741ed55360e
Steve Jeong <how2soft@gmail.com>
2023-02-15 17:20:16 +0900

    kit/src: Change adc minimum value and led init state.
    Signed-off-by: Steve Jeong <how2soft@gmail.com>
    Change-Id: Id3798dbaa9e1280938aab12949bf6762290c12de

--- kit/src/analog/analog-input.c
+++ kit/src/analog/analog-input.c
@@ -24,7 +24,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 /* adc */
 #define ADC_MAX 4095 // 4KB - 1
-#define ADC_MIN (SENSING_RANGE + 5)
+#define ADC_MIN 0
 
 #define ADC 29
 #define LED 24
@@ -46,6 +46,7 @@ int main()
 	pinMode(LED, OUTPUT);
 
 	prev_value = analogRead(ADC);
+	digitalWrite(LED, LOW);
 
 	while (1) {
 		value = analogRead(ADC);
