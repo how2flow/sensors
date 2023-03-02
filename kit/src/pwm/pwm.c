18b33ff109676ecff74fbc90db94a53c4e6e627a
Steve Jeong <how2soft@gmail.com>
2023-03-02 15:37:10 +0900

    kit/src: update pwm codes.
    rename codes and change pwm 1ch example.
    
    Signed-off-by: Steve Jeong <how2soft@gmail.com>
    Change-Id: Ieb7a0b7c6369463f201bdc896224508603a887ad

--- kit/src/pwm/pwm.c
+++ kit/src/pwm/pwm.c
@@ -18,42 +18,19 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 #include <wiringPi.h>
 #include <stdio.h>
 
-#define PIN 7
 #define PWM 23
-
-#define MAX 100000
-#define DIV (MAX / 100)
+#define DUTY 50
 
 /* target:
  */
 
 int main()
 {
-	int value;
-	int brightness;
-
 	wiringPiSetup();
-	pinMode(PIN, INPUT);
 	pinMode(PWM, PWM_OUTPUT);
 	pwmSetClock(2400);
 	pwmSetRange(100);
-
-	brightness = MAX;
-
-	while (1) {
-		value = digitalRead(PIN);
-		if (!value) {
-			if (brightness <= MAX)
-				brightness--;
-		}
-		else {
-			if (brightness >= 0)
-				brightness++;
-		}
-		if (brightness < 0)
-			brightness = 0;
-		pwmWrite(PWM, brightness);
-	}
+	pwmWrite(PWM, DUTY);
 
 	return 0;
 }
